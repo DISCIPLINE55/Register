3e24e3501b4007e82a33a21af37ff8a71f402b0c
Ismail Ibrahim Mensah <ismailmensah055@gmail.com>
2025-10-20 23:27:40 -0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'alkhulafau-cache-v1';
+const ASSETS = [
+  './', './index.html', './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
